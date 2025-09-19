1cb10384e3571326b06e9bd51e7a14ec8717eb7f
Aryan Kesarwani <93967099+Aryankesarwani@users.noreply.github.com>
2025-09-19 19:34:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	if(arr.size() == 1) return arr[0];
+	arr=arr.map(Number);
+	arr.sort((a,b)=>a-b);
+	min=0;
+	const ans = arr.forEach(item,i)=>{
+		min+=item;
+		return min;
+	}
+	ans.shift();
+	return mincost(ans);
   
 }
 
